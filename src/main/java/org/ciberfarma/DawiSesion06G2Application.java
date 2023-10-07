67c2dbd2f919edfe61916b281afba5c457c009a8
Elias <elias.alejandro21@gmail.com>
2023-10-06 19:51:46 -0500

    Primer commit

---
+++ src/main/java/org/ciberfarma/DawiSesion06G2Application.java
@@ -0,0 +1,13 @@
+package org.ciberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiSesion06G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiSesion06G2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/ciberfarma/DawiSesion06G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ciberfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiSesion06G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
